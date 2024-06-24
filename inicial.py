8a15f81b4766a569cc7815619fe9262a18dd0176
kauafelipe188 <kauafelipe0172@gmail.com>
2024-06-24 17:12:46 -0300

    commit2

---
+++ inicial.py
@@ -0,0 +1,18 @@
+print("Olá mundo")
+
+print("estamos praticando a progamação")
+print("primeiro estamos praticando o comando print")
+print("este comando printa 3 linhas de texto")
+
+print("Meu nome Kaua")
+print("Minha casa")
+print(17)
+
+#meu primeiro python
+nome = input("Digite seu nome:")
+print("Olá, " + nome,"!")
+
+#String F
+resultado = 25
+resultado2 = 30
+print(f"O resultado é {resultado}, e a conta dos dois valores é{resultado2 + resultado}")
