5d5ebd0db636bbd6a648f9a1a7278e8598e2d593
kauafelipe188 <kauafelipe0172@gmail.com>
2024-06-25 17:04:17 -0300

    commit2

---
+++ atividades.py
@@ -0,0 +1,42 @@
+#atividade1
+nome = input("Digite seu nome:")
+apelido = input("Digite seu apelido:")
+endereço = input("digite seu endereço:")
+cidpos = input("Digite sua cidade e codigo postal:")
+##chamando inputs e printando
+print(nome,apelido,endereço,cidpos)
+
+#atividade2
+nome1 = input("digite um nome:")
+ano = input("digite um ano:")
+print (nome1,"é um/a valente cavaleir@ nascid@ no ano de", ano , nome1, "acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente", nome1, "kauapoderia salvar os moradores da aldeia")
+
+#atividade3
+cidade = "sao paulo"
+
+print(cidade)
+
+cidade = "rio de janeiro"
+print(cidade)
+
+#atividade4
+string = input("digite uma string:")
+num = input("digite um numero inteiro:")
+float = input("digite um numero irracional:")
+print("string:",string)
+print("inteiro:",num)
+print("irracional:",float)
+
+#atividade5
+fra1 = "se juntar"
+fra2 = "com a outra"
+print(fra1 + fra2)
+
+#atividade6
+numero = int(input("coloque um numero:"))
+print(f"o resultado de {numero} vezes 5 é {5 * numero}")
+
+#atividade7
+ano = int(input("coloque seu ano de nascimento aqui: "))
+nome = input("coloque seu nome: ")
+print(f"Olá {nome}, voce fará {2024 - ano} anos ao final de 2024")
--- inicial.py
+++ inicial.py
@@ -1,18 +1,40 @@
-print("Olá mundo")
+#print("Olá mundo")
 
-print("estamos praticando a progamação")
-print("primeiro estamos praticando o comando print")
-print("este comando printa 3 linhas de texto")
+#print("estamos praticando a progamação")
+#print("primeiro estamos praticando o comando print")
+#print("este comando printa 3 linhas de texto")
 
-print("Meu nome Kaua")
-print("Minha casa")
-print(17)
+#print("Meu nome Kaua")
+#print("Minha casa")
+#print(17)
 
 #meu primeiro python
-nome = input("Digite seu nome:")
-print("Olá, " + nome,"!")
+#nome1 = input("Digite seu nome:")
+#print("Olá, " + nome1,"!")
 
 #String F
-resultado = 25
-resultado2 = 30
-print(f"O resultado é {resultado}, e a conta dos dois valores é{resultado2 + resultado}")
+#resultado = 25
+#resultado2 = 30
+#print(f"O resultado é {resultado}, e a conta dos dois valores é  {resultado2 + resultado}")
+
+#nome = "kaua felipe"
+#idade = 17
+#skill1 = "python"
+#nivel1 = "iniciante"
+#skill2 = "html"
+#nivel2 = "junior"
+#skill3 = "JavaScript"
+#nivel3 = "iniciante"
+#minimo = 3000
+#maximo = "∞"
+#print(f"Olá, eu sou {nome} e tenho {idade} anos. Minhas habilidades são {skill1} no nível {nivel1}, {skill2} no nível {nivel2} e {skill3} no nível {nivel3}. Procuro um emprego que pague entre #{minimo} e {maximo} reais por mês.")
+
+#ano = int(input("coloque seu ano de nascimento aqui: "))
+#print(f"ao final de 2024 voce ter {2024 - ano} anos")
+
+altura = float(input("qual a sua altura?"))
+peso = float(input("qual o seu peso?"))
+
+imc = peso / (altura / 100) ** 2
+
+print(f"seu imc é {imc:.2f}")
