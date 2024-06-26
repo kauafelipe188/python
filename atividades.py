688bb4f66be2f6b5ad577d7597a4ceecca9b9e1a
kauafelipe188 <kauafelipe0172@gmail.com>
2024-06-26 15:00:16 -0300

    commit26

--- atividades.py
+++ atividades.py
@@ -1,42 +1,59 @@
 #atividade1
-nome = input("Digite seu nome:")
-apelido = input("Digite seu apelido:")
-endereço = input("digite seu endereço:")
-cidpos = input("Digite sua cidade e codigo postal:")
+#nome = input("Digite seu nome:")
+#apelido = input("Digite seu apelido:")
+#endereço = input("digite seu endereço:")
+#cidpos = input("Digite sua cidade e codigo postal:")
 ##chamando inputs e printando
-print(nome,apelido,endereço,cidpos)
+#print(nome,apelido,endereço,cidpos)
 
 #atividade2
-nome1 = input("digite um nome:")
-ano = input("digite um ano:")
-print (nome1,"é um/a valente cavaleir@ nascid@ no ano de", ano , nome1, "acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente", nome1, "kauapoderia salvar os moradores da aldeia")
+#nome1 = input("digite um nome:")
+#ano = input("digite um ano:")
+#print (nome1,"é um/a valente cavaleir@ nascid@ no ano de", ano , nome1, "acordou com um barulho terrivel: um dragão se aproximava da aldeia. Somente", nome1, "kauapoderia salvar os moradores da aldeia")
 
 #atividade3
-cidade = "sao paulo"
+#cidade = "sao paulo"
 
-print(cidade)
+#print(cidade)
 
-cidade = "rio de janeiro"
-print(cidade)
+#cidade = "rio de janeiro"
+#print(cidade)
 
 #atividade4
-string = input("digite uma string:")
-num = input("digite um numero inteiro:")
-float = input("digite um numero irracional:")
-print("string:",string)
-print("inteiro:",num)
-print("irracional:",float)
+#string = input("digite uma string:")
+#num = input("digite um numero inteiro:")
+#float = input("digite um numero irracional:")
+#print("string:",string)
+#print("inteiro:",num)
+#print("irracional:",float)
 
 #atividade5
-fra1 = "se juntar"
-fra2 = "com a outra"
-print(fra1 + fra2)
+#fra1 = "se juntar"
+#fra2 = "com a outra"
+#print(fra1 + fra2)
 
 #atividade6
-numero = int(input("coloque um numero:"))
-print(f"o resultado de {numero} vezes 5 é {5 * numero}")
+#numero = int(input("coloque um numero:"))
+#print(f"o resultado de {numero} vezes 5 é {5 * numero}")
 
 #atividade7
-ano = int(input("coloque seu ano de nascimento aqui: "))
-nome = input("coloque seu nome: ")
-print(f"Olá {nome}, voce fará {2024 - ano} anos ao final de 2024")
+#ano = int(input("coloque seu ano de nascimento aqui: "))
+#nome = input("coloque seu nome: ")
+#print(f"Olá {nome}, voce fará {2024 - ano} anos ao final de 2024")
+
+#atividade8
+#preço = float(input("coloque o preço: "))
+#desconto = int(input("coloque a porcentagem do desconto: "))
+#print(f"o preço com desconto é {preço - (preço * desconto / 100)} reais")
+#print(f"o valor do desconto é {preço * desconto / 100} reais ")
+
+#atividade9
+#valor = int(input("coloque o valor da conta: "))
+#PercentualGorjeta = int(input("coloque o percentual da gorjeta: "))
+#print(f"o valor total é {valor + (valor * PercentualGorjeta / 100)} reais")
+
+#atividade10
+
+valor = int(input("valor em real: "))
+dolar = int(valor / 5.51)
+print(f"o valor em dolar é {dolar} dolares")
